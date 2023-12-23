32455ea8cf28dd04f6f940ba14f55b6bffef8692
Hugo Latendresse <hugolatendresse@gmail.com>
2023-12-22 23:52:05 -0500

    kernel.c

---
+++ os/kernel/kernel.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "syscalls.h"
+
+void kernel_main() {
+    /* Initialize hardware, drivers, etc. */
+
+    /* Enter an infinite loop */
+    while (1) {
+        /* Handle system calls, interrupts, etc. */
+    }
+}
